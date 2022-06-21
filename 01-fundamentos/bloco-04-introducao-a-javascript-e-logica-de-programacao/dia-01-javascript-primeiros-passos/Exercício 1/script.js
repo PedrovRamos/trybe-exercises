ac6576951559827c7acf6c96693d0743ff9e6d79
PedrovRamos <pvramos.cunha@gmail.com>
2022-06-21 15:37:38 -0300

    Adicionando exercicio 1 dia 4.1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercício 1/script.js
@@ -0,0 +1,17 @@
+const myName = "Pedro";
+const birthCity = "Niterói";
+var birthYear = 2004
+
+console.log(myName, birthCity, birthYear)
+
+birthYear = 2030
+
+console.log(birthYear)
+
+// const birthCity = "São Gonçalo";
+
+// console.log(birthCity)
+
+// O console retornaria uma mensagem de erro, pois a constante já foi declarada anteriormente e não é possível alterar o valor de uma constante.
+
+
