e8d50010d8aef190b9e3fb25e9920f51a92f515a
PedrovRamos <pvramos.cunha@gmail.com>
2022-06-24 00:46:55 -0300

    Adicionando exercício do dia 4.3 incompleto

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercício do dia/script.js
@@ -0,0 +1,22 @@
+// Programa 01
+
+let fatorialDez = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 
+
+console.log(fatorialDez)
+
+// Programa 02 (Falta fazer)
+
+let word = 'tryber';
+let wordInv;
+
+for (let index = word.length - 1 ; index >= 0; index -= 1){
+    wordInv = wordInv + word[index]
+}
+
+console.log(wordInv)
+
+// Programa 03 (Falta fazer)
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Programa 04 (Falta fazer)
