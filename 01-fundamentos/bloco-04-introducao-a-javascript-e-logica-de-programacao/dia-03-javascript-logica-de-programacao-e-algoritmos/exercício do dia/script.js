1ecae6ebb4f9d83dc70a40cd654d6f1233a823c6
PedrovRamos <pvramos.cunha@gmail.com>
2022-06-24 13:57:22 -0300

    Adicionando exercício do dia 4.3 completo

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercício do dia/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercício do dia/script.js
@@ -4,19 +4,75 @@ let fatorialDez = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2
 
 console.log(fatorialDez)
 
-// Programa 02 (Falta fazer)
+// Programa 02 
 
-let word = 'tryber';
-let wordInv;
+let word = 'banana';
+let inv = ""
 
-for (let index = word.length - 1 ; index >= 0; index -= 1){
-    wordInv = wordInv + word[index]
+for (let index = word.length - 1; index >= 0; index -= 1 ){
+    inv = inv + word[index]
 }
 
-console.log(wordInv)
+console.log(inv)
 
-// Programa 03 (Falta fazer)
+// Programa 03
+
+
+// Algoritmo 01
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-// Programa 04 (Falta fazer)
+let lgth = 0
+let longest;
+
+for (let index = 0; index < array.length; index += 1){
+
+    if (array[index].length > lgth) {
+        lgth = array[index].length;
+        longest = array[index];
+    }
+
+}
+
+console.log(longest)
+
+// Algoritmo 02
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let lgth = 100;
+let short;
+
+for (let index = 0; index < array.length; index += 1){
+
+    if (array[index].length < lgth) {
+        lgth = array[index].length;
+        short = array[index];
+    }
+
+}
+
+console.log(short)
+      
+      
+
+// Programa 04 
+
+let primos = [2]
+
+for (let index = 3; index <= 50; index += 1){
+    
+    let divisores = 0
+
+    for (let i = 2; i < index; i += 1){
+        if(index % i == 0){
+            divisores += 1
+        }
+    }
+
+    if (divisores == 0){
+        primos.push(index)
+    }
+}
+
+console.log(Math.max(...primos))
