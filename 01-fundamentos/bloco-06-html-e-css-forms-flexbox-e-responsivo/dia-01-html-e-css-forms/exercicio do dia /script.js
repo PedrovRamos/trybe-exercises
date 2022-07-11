945f8844756bb0b503cd8d6e698de566cf23057c
PedrovRamos <pvramos.cunha@gmail.com>
2022-07-11 18:23:58 -0300

    Adicionando exercício do dia 6.2 e atualizando exercício do dia 6.1

---
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicio do dia /script.js
@@ -0,0 +1,5 @@
+// código para interromper o comportamento padrão do botão submit utilizando o método preventDefault().
+
+// document.getElementById("submit-button").addEventListener("click", function (event){
+//     event.preventDefault()
+// })
